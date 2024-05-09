23b6bc374e1b14791e39f38177efe7a8ded71742
FelipeVilas <167373812+FelipeVilas@users.noreply.github.com>
2024-05-09 17:27:28 -0300

    Deixando o código mais bonito

--- Validar_CEP.java
+++ Validar_CEP.java
@@ -9,7 +9,7 @@ public class Validar_CEP {
    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Digite o CEP: ");
+        System.out.print("Digite o CEP:");
         String cep = sc.nextLine();
 
         if (validarCEP(cep)) {
@@ -17,12 +17,10 @@ public class Validar_CEP {
         } else {
             System.out.println("O CEP é inválido.");
         }
-
-        
     }
 
-    // Função para validar um CEP
+
     public static boolean validarCEP(String cep) {
-       // Verifica se o CEP possui 8 dígitos
        return cep.length() == 8;
-    }}
+    }
+}
